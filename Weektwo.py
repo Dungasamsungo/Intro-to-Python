a3657d16deee25dd6da7c90c457fe1c0f62acedd
dungasamsungo <sampsonngobi@gmail.com>
2023-09-19 09:28:14 -0300

    Simple math operations

---
+++ Weektwo.py
@@ -0,0 +1,15 @@
+# age = int(input('How old are you?:'))
+# next_year_age = age + 1
+# print(f'On your next birthday, you will be {next_year_age}\n')
+
+# egg_cartons = int(input('How many cartons of egg do you have?:'))
+# number_of_eggs = egg_cartons * 12
+# print(f'You have {number_of_eggs} eggs.\n')
+
+
+# number_cookies = int(input('How many cookies do you have?'))
+# number_of_poeple = int(input('How many people are there?'))
+# cookie_per_person = number_cookies // number_of_poeple 
+
+# print(f'Each person may have {cookie_per_person} cookies ')
+
