c518f4ec2e52b29cd15ae21db07d0b5bf37f529b
dungasamsungo <sampsonngobi@gmail.com>
2023-10-04 22:40:19 -0300

    core requirements 2 and three completed

--- week 4/TeamActivityGuessMyNumberGame.py
+++ week 4/TeamActivityGuessMyNumberGame.py
@@ -1,6 +1,7 @@
 import random
 
 
+# core requirements 1 
 
 magic_number =  int(input('What is the magic number? '))
 guess =  int(input('What is your guess? '))
@@ -8,10 +9,56 @@ guess =  int(input('What is your guess? '))
 if guess == magic_number:
     print('You guessed it!')
 
-elif guess > magic_number:
-    print('Lower')
+elif guess < magic_number:
+    print('Higer')
+    
 
 else:
-     print('Higer')
+    print('Lower')
+    
 
+
+# core requirements 2 
+
+magic_number =  int(input('What is the magic number? '))
+
+while True:
+    guess =  int(input('What is your guess? '))
+    if guess == magic_number:
+        print('You guessed it!')
+        break
+
+    elif guess < magic_number:
+        print('Higer')
+
+    else:
+        print('Lower')
+   
+
+# core requirements 3 
+
+magic_number = random.randint(10, 20)
+
+while True:
+    guess =  int(input('What is your guess? '))
+    if guess == magic_number:
+        print('You guessed it!')
+        break
+
+    elif guess < magic_number:
+        print('Higer')
+
+    else:
+        print('Lower')
+   
+
+
+   
+         
+
+             
+
+
+ 
+     
 
