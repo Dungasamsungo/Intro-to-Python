a06650f824e73a4ac5b8552ae34391d86f05ec6a
dungasamsungo <sampsonngobi@gmail.com>
2023-10-13 09:12:53 -0300

    Team activity completed

--- WEEK 5/TeamActivity.py
+++ WEEK 5/TeamActivity.py
@@ -18,6 +18,7 @@ while new_number != 0: # promt user for a number until 0 is entered
 
     if new_number != 0: #append all numbers to the numbers list expect 0
         numbers.append(new_number)
+        
 
 total = sum(numbers) # add all the numbers in the list
 print()
@@ -27,7 +28,7 @@ print()
 
 avereage = total / len(numbers) # calculate the average of the numbers in the list
 
-print(f'The sum is: {avereage}')
+print(f'The average is: {avereage}')
 print()
 
 largest_number = max(numbers) # find the highest or the maximum number in the list
@@ -35,5 +36,34 @@ largest_number = max(numbers) # find the highest or the maximum number in the li
 print(f'The largest number is: {largest_number}')
 print()
 
+# Stretch Challenge
+# Have the user enter both positive and negative numbers, then find the smallest positive number (the positive number that is closest to zero).
+# Sort the numbers in the list and display the new, sorted list. Hint: There are python libraries that can help you here, 
+# try searching the internet for them.
 
 
+smallest_number = None 
+for new_number in numbers:
+    if new_number > 0 and (smallest_number is None or new_number < smallest_number):
+        smallest_number = new_number
+
+if smallest_number is not None:
+        print(f'The smallest number is: {smallest_number}')
+else:
+    print('No positive numbers found in the list!')
+print()
+
+soreted_numbers = sorted(numbers)
+
+print('The soreted numbers are')
+
+for number in soreted_numbers:
+
+    print(number)
+print()
+
+
+
+
+
+    
